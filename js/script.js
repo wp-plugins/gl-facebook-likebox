bf6152681dee6c990f1a1fab0d690514ba512449
Simivar <Simivar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-28 18:06:25 +0000

    git-svn-id: https://plugins.svn.wordpress.org/gl-facebook-likebox/trunk@390249 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*
+Plugin Name: GL Facebook Likebox
+Plugin URI: http://simivar.net/plugins/gl-facebook-likebox/
+Description: Adds a great-lookin' Facebook Likebox to Your site.
+Author: Krystian 'Simivar' Marcisz
+Author URI: http://www.simivar.net/
+Version: 1.0.2
+Text Domain: glfl
+Domain Path: /lang/
+*/
+
+
+jQuery.noConflict();
+
+var lastlen = 0;
+
+jQuery(document).ready(function($) {
+	$("#facebook-right").hover(
+	function right(){
+		$(this).animate({ right: "0" }, {queue:false, duration:"normal"} );
+	},
+	function right(){
+		$(this).animate({ right: "-243" }, {queue:false, duration:"normal"} );
+	});
+	
+	$("#facebook-left").hover(
+	function(){
+		$(this).animate({ left: "0" }, {queue:false, duration:"normal"} );
+	},
+	function(){
+		$(this).animate({ left: "-243" }, {queue:false, duration:"normal"} );
+	});
+	
+});
