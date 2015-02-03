a02f3278013ab1acc8b3e80242693560b40b620e
Simivar <Simivar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-03 09:10:48 +0000

    javascript file update
    git-svn-id: https://plugins.svn.wordpress.org/gl-facebook-likebox/trunk@1081416 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gl-facebook-likebox.php
+++ gl-facebook-likebox.php
@@ -5,13 +5,13 @@ Plugin URI: http://simivar.net/plugins/gl-facebook-likebox/
 Description: Adds a great-lookin' Facebook Likebox to Your site.
 Author: Krystian 'Simivar' Marcisz
 Author URI: http://www.simivar.net/
-Version: 1.0.7
+Version: 1.0.8
 Text Domain: glfl
 Domain Path: /lang/
 */
 
 ## Define author and plugin version
-define('GLFL_VERSION', '1.0.7');
+define('GLFL_VERSION', '1.0.8');
 define('GLFL_AUTHOR', 'Krystian "Simivar" Marcisz');
 
 if ( !function_exists( 'glfl_show' ) ) :
@@ -80,7 +80,7 @@ if ( !function_exists( 'glfl_AdminStyles' ) ) :
 ## - admin page
 function glfl_AdminStyles(){
 	global $pluginurl;
-	wp_register_style( "glfl-adminstyle",  $pluginurl . 'css/admin.css', null, '1.0.7'); 
+	wp_register_style( "glfl-adminstyle",  $pluginurl . 'css/admin.css', null, '1.0.8'); 
 	wp_enqueue_style( 'glfl-adminstyle' );
 }
 endif;
@@ -89,7 +89,7 @@ if ( !function_exists( 'glfl_styles' ) ) :
 ## - blog page
 function glfl_styles(){
 	global $pluginurl;
-	wp_register_style( "glfl-styles",  $pluginurl . 'css/style.css', null, '1.0.7'); 
+	wp_register_style( "glfl-styles",  $pluginurl . 'css/style.css', null, '1.0.8'); 
 	wp_enqueue_style( 'glfl-styles' );
 }
 endif;
--- js/script.js
+++ js/script.js
@@ -4,26 +4,26 @@ Plugin URI: http://simivar.net/plugins/gl-facebook-likebox/
 Description: Adds a great-lookin' Facebook Likebox to Your site.
 Author: Krystian 'Simivar' Marcisz
 Author URI: http://www.simivar.net/
-Version: 1.0.7
+Version: 1.0.8
 Text Domain: glfl
 Domain Path: /lang/
 */
 
 jQuery(document).ready(function($) {
 	$("#facebook-right").hover(
-	function right(){
-		$(this).animate({ right: "0px" }, {queue:false, duration:"normal"} );
+	function(){
+		$(this).css('z-index', 200 ).animate({ right: "0px" }, {queue:false, duration:"normal"} );
 	},
-	function right(){
-		$(this).animate({ right: "-243px" }, {queue:false, duration:"normal"} );
+	function(){
+		$(this).animate({ right: "-243px", "z-index": "100" }, {queue:false, duration:"normal"} );
 	});
 	
 	$("#facebook-left").hover(
 	function(){
-		$(this).animate({ left: "0px" }, {queue:false, duration:"normal"} );
+		$(this).css('z-index', 200 ).animate({ left: "0px" }, {queue:false, duration:"normal"} );
 	},
 	function(){
-		$(this).animate({ left: "-243px" }, {queue:false, duration:"normal"} );
+		$(this).animate({ left: "-243px", "z-index": "100" }, {queue:false, duration:"normal"} );
 	});
 	
 });
